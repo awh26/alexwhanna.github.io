80cb388b8fc08b8c09b6f9e526606594d409cdaf
awh26 <alexwhanna1@gmail.com>
2019-12-29 22:15:29 -0500

    Update alexLanding.js

--- alexLanding.js
+++ alexLanding.js
@@ -29,64 +29,81 @@ $(document).ready(function(){
 			
 
 			// Get the <span> element that closes the modal
-			var span = document.getElementsByClassName("close");
+			// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
 
-			// When the user clicks on the button, open the modal 
-			function openTab(innertext) {
-				var modall = document.getElementById("modall");
-				var javaboxcontent = document.getElementById("javaContent");
-				var javascriptboxcontent = document.getElementById("javascriptContent");
-				var finalcutboxcontent = document.getElementById("finalcutContent");
-				var photoshopboxcontent = document.getElementById("photoshopContent");
-				var photographyboxcontent = document.getElementById("photographyContent");
+// When the user clicks on the button, open the modal 
+function openTab(innertext) {
+	var modall = document.getElementById("modall");
+	var javaboxcontent = document.getElementById("javaContent");
+	var javascriptboxcontent = document.getElementById("javascriptContent");
+	var pythonboxcontent = document.getElementById("pythonContent");
+	var finalcutboxcontent = document.getElementById("finalcutContent");
+	var photoshopboxcontent = document.getElementById("photoshopContent");
+	var photographyboxcontent = document.getElementById("photographyContent");
+	var resumepopout = document.getElementById("resumeContent");
 
-				document.getElementById("javaContent").style.display = "none";
-				javascriptboxcontent.style.display = "none";
-				finalcutboxcontent.style.display = "none";
-				photoshopboxcontent.style.display = "none";
-				photographyboxcontent.style.display = "none";
-				modall.style.display = "block";
 
-				var closeBtn;
+	javaboxcontent.style.display = "none";
+	javascriptboxcontent.style.display = "none";
+	pythonboxcontent.style.display = "none";
+	finalcutboxcontent.style.display = "none";
+	photoshopboxcontent.style.display = "none";
+	photographyboxcontent.style.display = "none";
+	resumepopout.style.display = "none";
 
-				if (innertext === 'javaExpand') {
-					javaboxcontent.style.display = "block";
-					closeBtn = span[0];
-				} else if (innertext === 'javascriptExpand') {
-					javascriptboxcontent.style.display = "block";
-					closeBtn = span[1];
-				} else if (innertext === 'finalcutExpand') {
-					finalcutboxcontent.style.display = "block";
-					closeBtn = span[2];
-				} else if (innertext === 'photoshopExpand') {
-					photoshopboxcontent.style.display = "block";
-					closeBtn = span[3];
-				} else if (innertext === 'photographyExpand') {
-					photographyboxcontent.style.display = "block";
-					closeBtn = span[4];
-				}
+	modall.style.display = "block";
 
-				closeBtn.onclick = function() {
-					modall.style.display = "none";
-					javaboxcontent.style.display = "none";
-					javascriptboxcontent.style.display = "none";
-					finalcutboxcontent.style.display = "none";
-					photoshopboxcontent.style.display = "none";
-					photographyboxcontent.style.display = "none";
-				}
-			}
+	var closeBtn;
 
-			// When the user clicks anywhere outside of the modal, close it
-			window.onclick = function(event) {
-				if (event.target == modall) {
-			   		modall.style.display = "none";
-			   		javaboxcontent.style.display = "none";
-					javascriptboxcontent.style.display = "none";
-					finalcutboxcontent.style.display = "none";
-					photoshopboxcontent.style.display = "none";
-					photographyboxcontent.style.display = "none";
-				}
-			}
+	if (innertext === 'javaExpand') {
+		javaboxcontent.style.display = "block";
+		closeBtn = span[0];
+	} else if (innertext === 'javascriptExpand') {
+		javascriptboxcontent.style.display = "block";
+		closeBtn = span[1];
+	} else if (innertext === 'pythonExpand') {
+		pythonboxcontent.style.display = "block";
+		closeBtn = span[2];
+	} else if (innertext === 'finalcutExpand') {
+		finalcutboxcontent.style.display = "block";
+		closeBtn = span[3];
+	} else if (innertext === 'photoshopExpand') {
+		photoshopboxcontent.style.display = "block";
+		closeBtn = span[4];
+	} else if (innertext === 'photographyExpand') {
+		photographyboxcontent.style.display = "block";
+		closeBtn = span[5];
+	} else if (innertext === 'resumeExpand') {
+		resumepopout.style.display = "block";
+		closeBtn = span[6];
+	}
+
+	closeBtn.onclick = function() {
+		modall.style.display = "none";
+		javaboxcontent.style.display = "none";
+		javascriptboxcontent.style.display = "none";
+		pythonboxcontent.style.display = "none";
+		finalcutboxcontent.style.display = "none";
+		photoshopboxcontent.style.display = "none";
+		photographyboxcontent.style.display = "none";
+		resumepopout.style.display = "none";
+	}
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modall) {
+   		modall.style.display = "none";
+   		javaboxcontent.style.display = "none";
+		javascriptboxcontent.style.display = "none";
+		pythonboxcontent.style.display = "none";
+		finalcutboxcontent.style.display = "none";
+		photoshopboxcontent.style.display = "none";
+		photographyboxcontent.style.display = "none";
+		resumepopout.style.display = "none";
+	}
+}
 
 
 			// Header onscroll ---------------------------------------------------------------------------------------------------------------------
